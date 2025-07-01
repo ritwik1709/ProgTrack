5f6d0e02f4164e84d5ec7504a5c969648bf8ad31
Ritwik Sudhakar Tat <ritwikt17@gmail.com>
2025-07-01 18:27:41 +0530

    updated axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:9000',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:9000',
 });
 
 // Add a request interceptor to attach the token
