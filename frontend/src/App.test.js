9675592ce646436b3cbbae952aecee50fa03c831
Ritwik Sudhakar Tat <ritwikt17@gmail.com>
2026-01-03 00:13:44 +0530

    updated app.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,12 +2,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
+test('renders ProgTrack app', () => {
   render(
     <MemoryRouter>
       <App />
     </MemoryRouter>
   );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appElement = screen.getByText(/ProgTrack/i);
+  expect(appElement).toBeInTheDocument();
 });
